282e909204b841b0140b439c0017205f2685a42a
Rodwin Pieterse <rodwinpieterse@Rodwin-Pieterse.local>
2023-03-12 19:33:04 +0200

    escape untrusted input

--- router.php
+++ router.php
@@ -1,15 +1,6 @@
 <?php
 
-// parse the url and extract it from any query that might follow
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require "routes.php";
 
 function routeToController($uri, $routes){
     if (array_key_exists($uri, $routes)){
@@ -19,10 +10,13 @@ function routeToController($uri, $routes){
     }
 }
 
-routeToController($uri, $routes);
-
 function abort($code = 404){ // sets a default code to be passed to func
     http_response_code($code);
     require "views/{$code}.php";
     die();
 }
+
+// parse the url and extract it from any query that might follow
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+routeToController($uri, $routes);
