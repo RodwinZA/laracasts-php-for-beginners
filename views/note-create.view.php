ace2ce5edfc9d56144bb6b57d84ed359ffd5e900
Rodwin Pieterse <rodwinpieterse@Rodwin-Pieterse.local>
2023-03-13 08:46:13 +0200

    intro to form validation

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -15,7 +15,15 @@
                                     <div>
                                         <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
                                         <div class="mt-2">
-                                            <textarea id="body" name="body" rows="3" class="mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6" placeholder="your note goes here..."></textarea>
+                                            <textarea id="body" name="body" rows="3" class="mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6" placeholder="your note goes here...">
+                                                <?= $_POST['body'] ?? '' ?>
+                                            </textarea>
+
+                                            <?php if (isset($errors['body'])) : ?>
+                                                <p class="text-red-500 text-sm mt-5">
+                                                    <?= $errors['body'] ?>
+                                                </p>
+                                            <?php endif; ?>
                                         </div>
                                     </div>
                                 </div>
