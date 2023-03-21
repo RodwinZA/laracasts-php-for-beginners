033321b7c87544134ef0c5875cf663a377ef9a02
Rodwin Pieterse <rodwinpieterse@Rodwin-Pieterse.local>
2023-03-21 16:02:51 +0200

    one request one controller

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -13,7 +13,6 @@ $note = $db->query('select * from notes where id = :id', [
 
 authorize($note['user_id'] === $currentUserId);
 
-
 $db->query("delete from notes where id = :id", [
     'id' => $_POST['id']
 ]);
